fb9f33c32ad6a7d5b14bb2c8c01d45004873dc30
wiscaksono <wwicaksono96@gmail.com>
2022-01-07 05:25:00 +0700

    up

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        naraja: "#F55C30",
+        azul: "#03B3C2",
+        "soft-black": "#343434",
+        "soft-white": "#E5E5E5",
+      },
+      backgroundImage: {
+        pattern: "url('/image/patternbg.svg')",
+      },
+    },
+  },
+  plugins: [],
+};
